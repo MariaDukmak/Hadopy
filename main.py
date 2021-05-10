c532caa7123b366aabb9eadaa32e787f484b170f
Lucas van der Horst <Lucas@vdrHorst.nl>
2021-05-11 00:33:18 +0200

    Implemented parallel mapping and reducer worker

--- main.py
+++ main.py
@@ -1,42 +1,52 @@
 import sys
 import subprocess
 import click
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
+from typing import List
+
+Command = List[str]
+ENCODING = 'ascii'
 
 # bin/hadoop jar share/hadoop/tools/lib/hadoop-*streaming*.jar -file ~/mapper.py -mapper ~/mapper.py -file ~/reducer.py -reducer ~/reducer.py -input ~/tmp -output ~/output
 # cat columbus.txt | ./mapper_lf.py | sort -n | ./reduce_lf.py
+# cat example/products.txt | python main.py --mapper "python example/mapper.py" --reducer "python example/reducer.py"
+
+
+def mapper_worker(text: str, mapper: Command) -> str:
+    return subprocess.run(mapper,
+                          stdout=subprocess.PIPE,
+                          input=text,
+                          encoding=ENCODING
+                          ).stdout
+
+
+def reducer_worker(texts: List[str], reducer: Command) -> str:
+    concatenated = '\n'.join(sorted(filter(lambda x: '\t' in x, '\n'.join(texts).split('\n'))))
+
+    return subprocess.run(reducer,
+                          stdout=subprocess.PIPE,
+                          input=concatenated,
+                          encoding=ENCODING
+                          ).stdout
+
 
 @click.command()
 @click.option('--mapper', '--m', prompt='Mapper command', help='Mapper command, for example "python mapper.py"')
 @click.option('--reducer', '--r', prompt='Reducer command', help='Reducer command, for example "python reducer.py"')
-@click.option('--n_threads', prompt="Number of threads", help="Number of threads to use")
+@click.option('--n_threads', help="Number of threads to use", default=cpu_count())
 def cli(mapper, reducer, n_threads):
+    mapper: Command = mapper.split(' ')
+    reducer: Command = reducer.split(' ')
+
     line_split = sys.stdin.read().split('\n')
+
     with Pool(n_threads) as pool:
-        pool.map(
-            lambda lines: subprocess.Popen(mapper.split(' '),
-                                           tdout=subprocess.PIPE,
-                                           stderr=subprocess.STDOUT,
-                                           stdin=),
-            [line_split[i::n_threads] for i in range(n_threads)]
+        mapped = pool.starmap(
+            mapper_worker,
+            map(lambda i: ('\n'.join(line_split[i::n_threads]), mapper), range(n_threads))
         )
-        mapped = subprocess.Popen(mapper.split(' '),
-                                  stdout=subprocess.PIPE,
-                                  stderr=subprocess.STDOUT,
-                                  stdin=sys.stdin)
 
-    # Unfortunately "sorted" was already taken
-    ordered = subprocess.Popen(['sort'],
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT,
-                             stdin=mapped.stdout)
-
-    reduced = subprocess.Popen(reducer.split(' '),
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.STDOUT,
-                               stdin=ordered.stdout)
-
-    click.secho(reduced.communicate()[0].decode())
+    sys.stdout.write(reducer_worker(mapped, reducer))
 
 
 if __name__ == "__main__":
