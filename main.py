c7f6ea685b79afe3cf7146788aee568e2bc8d58e
Maria Dukmak <62339103+MariaDukmak@users.noreply.github.com>
2021-05-10 14:08:18 +0200

    First setup of the project.

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+import click
+
+# bin/hadoop jar share/hadoop/tools/lib/hadoop-*streaming*.jar -file ~/mapper.py -mapper ~/mapper.py -file ~/reducer.py -reducer ~/reducer.py -input ~/tmp -output ~/output
+
+@click.command()
+@click.option('--mapper', '--m', prompt='Path to mapper program', help='Path to mapper program')
+@click.option('--reducer', '--r', prompt='Path to reducer program', help='Path to reducer program')
+def parse(mapper, reducer):
+    click.secho(f"{mapper=}, {reducer=}", fg="blue", bold=True)
+    print(sys.stdin)
+
+
+if __name__ == "__main__":
+    parse()
